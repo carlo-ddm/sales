899b16c66e1b482759c9f62c0aa3a7490a97fdfe
carlo-ddm <carloduminuco90@gmail.com>
2023-07-01 14:14:39 +0200

    bug trovato --> Aggiunta dell' ultima voce di input 3 tra i prodotti esentasse. Aggiunta soluzione x controllo posizione di imported (da testare)

--- constants/ExemptProduct.java
+++ constants/ExemptProduct.java
@@ -4,6 +4,7 @@ public enum ExemptProduct {
     BOOK("book"),
     CHOCOLATE_BAR("chocolate bar"),
     BOX_OF_CHOCOLATES("box of chocolates"),
+    BOX_OF_IMPORTED_CHOCOLATES("box of imported chocolates"),
     PACKET_OF_HEADACHE_PILLS("packet of headache pills");
 
     private final String productName;
